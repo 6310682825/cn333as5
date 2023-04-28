354fd1b01dc3a60e6c7af6be8208840d95e121c3
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-04-28 21:40:06 +0700

    Add basic query functionalities

---
+++ app/src/main/java/com/example/phonebook/database/PhoneDao.kt
@@ -0,0 +1,25 @@
+package com.example.phonebook.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface PhoneDao {
+
+    @Query("SELECT * FROM PhoneDbModel")
+    fun getAllSync(): List<PhoneDbModel>
+
+    @Query("SELECT * FROM PhoneDbModel WHERE id IN (:phoneIds)")
+    fun getPhoneByIdSync(phoneIds: List<Long>): List<PhoneDbModel>
+
+    @Query("SELECT * FROM PhoneDbModel WHERE id LIKE :id")
+    fun findByIdSync(id: Long): PhoneDbModel
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(phoneDbModel: PhoneDbModel)
+
+    @Query("DELETE FROM PhoneDbModel WHERE id LIKE :id")
+    fun delete(id: Long)
+}
