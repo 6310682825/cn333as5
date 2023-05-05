4ea7d0a0d12e01da0156744f1f63db46d9742bd9
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-05-05 17:45:15 +0700

    Create app routing

---
+++ app/src/main/java/com/example/phonebook/routing/PhoneBooksRouter.kt
@@ -0,0 +1,20 @@
+package com.example.phonebook.routing
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+
+sealed class Screen {
+    object Phones: Screen()
+    object SavePhone: Screen()
+    object Trash: Screen()
+}
+
+object PhoneBooksRouter {
+    var currentScreen: Screen by mutableStateOf(Screen.Phones)
+
+    fun navigateTo(destination: Screen) {
+        currentScreen = destination
+    }
+}
