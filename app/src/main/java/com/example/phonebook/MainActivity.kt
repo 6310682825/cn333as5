c0a6d7c25c8f3785e7239fc44f92b668d1477086
Thunyavat Thanakornsombat <6310682825@student.tu.ac.th>
2023-05-05 17:46:46 +0700

    Finish project

--- app/src/main/java/com/example/phonebook/MainActivity.kt
+++ app/src/main/java/com/example/phonebook/MainActivity.kt
@@ -1,43 +1,51 @@
 package com.example.phonebook
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.phonebook.routing.PhoneBooksRouter
+import com.example.phonebook.routing.Screen
+import com.example.phonebook.screens.PhonesScreen
+import com.example.phonebook.screens.SavePhoneScreen
+import com.example.phonebook.screens.TrashScreen
 import com.example.phonebook.ui.theme.PhoneBookTheme
+import com.example.phonebook.viewmodel.MainViewModel
+import com.example.phonebook.viewmodel.MainViewModelFactory
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PhoneBookTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val viewModel: MainViewModel = viewModel(
+                    factory = MainViewModelFactory(LocalContext.current.applicationContext as Application)
+                )
+                MainActivityScreen(viewModel)
             }
         }
     }
 }
 
+@ExperimentalMaterialApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PhoneBookTheme {
-        Greeting("Android")
+fun MainActivityScreen(viewModel: MainViewModel) {
+    Surface {
+        when (PhoneBooksRouter.currentScreen) {
+            is Screen.Phones -> PhonesScreen(viewModel)
+            is Screen.SavePhone -> SavePhoneScreen(viewModel)
+            is Screen.Trash -> TrashScreen(viewModel)
+        }
     }
 }
